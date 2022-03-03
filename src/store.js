812e3c0e69414f47b7848b253062d54eab8676eb
sjk <jkshin0903@gmail.com>
2022-03-03 22:43:42 +0900

    createAction으로 dispatch할 action 재정의

--- src/store.js
+++ src/store.js
@@ -1,20 +1,26 @@
 import { createStore } from 'redux'
+import { createAction } from '@reduxjs/toolkit'
 
-const addTodo = (text) => ({
-  type: 'ADD_TO_DO',
-  text,
-})
-const deleteTodo = (id) => ({
-  type: 'DELETE_TO_DO',
-  id,
-})
+// const addTodo = (text) => ({
+//   type: 'ADD_TO_DO',
+//   text,
+// })
+// const deleteTodo = (id) => ({
+//   type: 'DELETE_TO_DO',
+//   id,
+// })
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TO_DO':
-      return [{ text: action.text, id: Date.now() }, ...state]
-    case 'DELETE_TO_DO':
-      return state.filter((el) => el.id !== +action.id)
+// redux toolkit에서 action을 정의할 때는 createActon 메서드를 사용한다.
+// dispatch를 발생할 때 parameter로 전달한 데이터는 payload라는 속성으로 전달된다.
+const addTodo = createAction('ADD_TO_DO')
+const deleteTodo = createAction('DELETE_TO_DO')
+
+const reducer = (state = [], { type, payload }) => {
+  switch (type) {
+    case addTodo.type:
+      return [{ text: payload, id: Date.now() }, ...state]
+    case deleteTodo.type:
+      return state.filter((el) => el.id !== +payload)
     default:
       return state
   }
